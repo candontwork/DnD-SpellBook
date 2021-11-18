e6a7c1dbb66a4de585567abe66f4b28a1dfd2e32
candontwork <www.candontwork@gmail.com>
2021-11-18 22:27:33 +0800

    navBar updated

--- spellbook/src/components/NavBar.jsx
+++ spellbook/src/components/NavBar.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Nav from "react-bootstrap/Nav"
+import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
   return (
     <>
-  <Nav className="justify-content-center" activeKey="/home">
-    <Nav.Item>
-      <Nav.Link href="/home">Home</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href ="/SpellBook" eventKey="link-1">SpellBook</Nav.Link>
-    </Nav.Item>
-  </Nav>
+      <Nav fill variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/SpellBook" eventKey="link-1">
+            SpellBook
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </>
   );
 }
